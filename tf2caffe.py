ed9ec6098b82d8fd764644041ac4b24430ddd1ae
abner2015 <abner2015@qq.com>
2017-08-11 11:48:01 +0800

    Update tf2caffe.py
    add tf2caffe function

--- tf2caffe.py
+++ tf2caffe.py
@@ -1,4 +1,4 @@
-import sys, argparse
+import sys
 import tensorflow as tf
 import caffe
 import numpy as np
@@ -20,44 +20,46 @@ def tensor4d_transform(tensor):
 def tensor2d_transform(tensor):
     return tensor.transpose((1, 0))
 
-for key_i in var_to_shape_map:
+def tf2caffe(checkpoint_path,cf_prototxt,cf_model):
+    for key_i in var_to_shape_map:
 
-    try:
+        try:
 
-        if 'data' in key_i:
-            pass
-        elif 'weights' in key_i:
-            a = key_i.split('/')
-            if(len(a)==3):
-                key_caffe = a[1]
-            else:
-                key_caffe = a[2]
-            if(reader.get_tensor(key_i).ndim == 4):
-                if(key_caffe == 'fc6'):
-                    weights = tensor4d_transform(reader.get_tensor(key_i).reshape([7,7,512,4096])).reshape([[7,7,512,4096][3], -1])
-                elif key_caffe == 'fc7':
-                    a = np.squeeze(reader.get_tensor(key_i)[0][0])
-                    weights = tensor2d_transform(a)#2dim
-                elif key_caffe == 'fc8':
-                    a = np.squeeze(reader.get_tensor(key_i)[0][0])
-                    weights = tensor2d_transform(a)#2dim
+            if 'data' in key_i:
+                pass
+            elif 'weights' in key_i:
+                a = key_i.split('/')
+                if (len(a) == 3):
+                    key_caffe = a[1]
                 else:
-                    weights = tensor4d_transform(reader.get_tensor(key_i))
-            net.params[key_caffe][0].data.flat = weights.flat
-        elif 'biases' in key_i:
-            a = key_i.split('/')
-            if (len(a) == 3):
-                key_caffe = a[1]
+                    key_caffe = a[2]
+                if (reader.get_tensor(key_i).ndim == 4):
+                    if (key_caffe == 'fc6'):
+                        weights = tensor4d_transform(reader.get_tensor(key_i).reshape([7, 7, 512, 4096])).reshape(
+                            [[7, 7, 512, 4096][3], -1])
+                    elif key_caffe == 'fc7':
+                        a = np.squeeze(reader.get_tensor(key_i)[0][0])
+                        weights = tensor2d_transform(a)  # 2dim
+                    elif key_caffe == 'fc8':
+                        a = np.squeeze(reader.get_tensor(key_i)[0][0])
+                        weights = tensor2d_transform(a)  # 2dim
+                    else:
+                        weights = tensor4d_transform(reader.get_tensor(key_i))
+                net.params[key_caffe][0].data.flat = weights.flat
+            elif 'biases' in key_i:
+                a = key_i.split('/')
+                if (len(a) == 3):
+                    key_caffe = a[1]
+                else:
+                    key_caffe = a[2]
+                net.params[key_caffe][1].data.flat = reader.get_tensor(key_i).flat
+            elif 'mean_rgb' in key_i:
+                pass
+            elif 'global' in key_i:
+                pass
             else:
-                key_caffe = a[2]
-            net.params[key_caffe][1].data.flat = reader.get_tensor(key_i).flat
-        elif 'mean_rgb' in key_i:
-            pass
-        elif 'global' in key_i:
-            pass
-        else:
-            sys.exit("Warning!  Unknown tf:{}".format(key_i))
-    except KeyError:
-        print("\nWarning!  key error mxnet:{}".format(key_i))
-net.save(cf_model)
-print("\n- Finished.\n")
+                sys.exit("Warning!  Unknown tf:{}".format(key_i))
+        except KeyError:
+            print("\nWarning!  key error tf:{}".format(key_i))
+    net.save(cf_model)
+    print("\n- Finished.\n")
